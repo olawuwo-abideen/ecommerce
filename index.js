d9c9126133eed212bd872e3b4f907112ae1e1ebb
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-07-05 11:48:12 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const connectDB = require('./db/connect');
+
+const app = express();
+
+
+
+
+// middleware
+app.use(express.json());
+
+// routes
+
+app.get('/', (req, res) => {
+  res.send('<h1>ecommerce API</h1>');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const port = process.env.PORT || 3000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
