817a9e99f7bc668cc8867d33e323c3ee842ff76c
abideen-olawuwo <abideenolawuwo@gmail.com>
2023-07-12 14:07:33 +0100

    fix

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 require('dotenv').config();
+require('express-async-errors');
+ 
+//Express
 const express = require('express');
-const mongoose = require('mongoose');
-const connectDB = require('./db/connect');
-
 const app = express();
 
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const fileUpload = require('express-fileupload');
+const rateLimiter = require('express-rate-limit');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const cors = require('cors');
+const mongoSanitize = require('express-mongo-sanitize');
 
+
+//Database
+const connectDB = require('./db/connect');
 
 
 // middleware
@@ -17,17 +28,39 @@ app.get('/', (req, res) => {
   res.send('<h1>ecommerce API</h1>');
 });
 
+const authRouter = require('./routes/authRoutes');
+const userRouter = require('./routes/userRoutes');
+const productRouter = require('./routes/productRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
+const orderRouter = require('./routes/orderRoutes');
 
+// middleware
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 60,
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(mongoSanitize());
 
+app.use(express.json());
+app.use(cookieParser(process.env.JWT_SECRET));
 
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/products', productRouter);
+app.use('/api/v1/reviews', reviewRouter);
+app.use('/api/v1/orders', orderRouter);
 
-
-
-
-
-
-
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 
 
