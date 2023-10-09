df074cb5a6d0e9bb86f13a0dd14ea32367aaaee0
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-10-09 09:38:37 +0100

    add rate limiter

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,8 +3,18 @@ const router = express.Router();
 
 const { register, login, logout } = require('../controllers/auth');
 
-router.post('/register', register);
-router.post('/login', login);
+const rateLimiter = require('express-rate-limit');
+
+const apiLimiter = rateLimiter({
+  windowMs: 15 * 60 * 1000,
+  max: 10,
+  message: {
+    msg: 'Too many requests from this IP, please try again after 15 minutes',
+  },
+});
+
+router.post('/register', apiLimiter, register);
+router.post('/login', apiLimiter, login);
 router.get('/logout', logout);
 
 module.exports = router;
