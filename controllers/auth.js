6544323918978078cd3cc4319141951bbe2243fb
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-10-09 10:07:21 +0100

    fix

--- controllers/auth.js
+++ controllers/auth.js
@@ -12,7 +12,7 @@ const register = async (req, res) => {
   }
 
   // first registered user is an admin
-  const isFirstAccount = (await User.countDocuments({})) === 0;
+  const isFirstAccount = (await User.accountDocuments({})) === 0;
   const role = isFirstAccount ? 'admin' : 'user';
 
   const user = await User.create({ name, email, password, role });
